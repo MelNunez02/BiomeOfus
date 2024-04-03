c0431a70e298766639f437a642be41dcb8ae328f
Sergio H Nunez <shnunez@o1devs.com>
2024-04-03 09:57:48 -0500

    Initial Commit

---
+++ BiomeOfus/BiomeOfusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiomeOfusApp.swift
+//  BiomeOfus
+//
+//  Created by Sergio Nunez on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BiomeOfusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiomeOfus/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  BiomeOfus
+//
+//  Created by Melanie Nunez on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private var data: [Int] = Array(1...20)
+    private let colors: [Color] = [.red, .green, .blue, .yellow]
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 170))
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                    ForEach(data, id: \.self) { number in
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 170, height: 170)
+                                .foregroundColor(colors[number%4])
+                                .cornerRadius(30)
+                            Text("\(number)")
+                                .foregroundColor(.white)
+                                .font(.system(size: 80, weight: .medium, design: .rounded))
+                        }
+                    }
+                    
+                }
+            }
+            .padding()
+            .navigationTitle("Dashboard")
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}

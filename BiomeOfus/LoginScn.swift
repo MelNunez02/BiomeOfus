575f9f033ef80f6b7b9f469b33285c7c4aa9d32c
Melanie Nunez <Melaniejnunez@gmail.com>
2024-04-17 21:27:21 -0500

    melscode- "login screen done".

---
+++ BiomeOfus/LoginScn.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScn.swift
+//  BiomeOfus
+//
+//  Created by Melanie Nunez on 4/17/24.
+//
+
+import SwiftUI
+
+struct LoginScn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScn()
+}

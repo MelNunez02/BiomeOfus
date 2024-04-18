e14d9b1bd1e11fb1f5379d727c559079764c5100
Jaresi Nunez <jaresinunez@gmail.com>
2024-04-18 14:40:46 -0500

    develop - BiomeForm in progress

--- BiomeOfus/BiomeForm.swift
+++ BiomeOfus/BiomeForm.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct BiomeForm: View {
+    @AppStorage("BTitle") private var title: String = "Book Title" // Default title
+    @State private var paragraphText: String = ""
+    
+    
     var body: some View {
         ZStack {
             Image("Background")
@@ -19,18 +23,66 @@ struct BiomeForm: View {
             
             VStack {
                 
-                Text("Book Title")
-                    .fontWeight(.black)
-                    .foregroundColor(Color(red: 244/255, green: 252/255, blue: 3/255))
-                    .font(.system(size: 35))
-                    .font(.largeTitle .uppercaseSmallCaps())
-                    .shadow(color: .black, radius: 13, x: 3, y: 3)
-                    .padding(.top, -330)
-                    .offset(x: -45)
+//                Text("Book Title")
+//                    .fontWeight(.black)
+//                    .foregroundColor(Color(red: 244/255, green: 252/255, blue: 3/255))
+//                    .font(.system(size: 35))
+//                    .font(.largeTitle .uppercaseSmallCaps())
+//                    .shadow(color: .black, radius: 13, x: 3, y: 3)
+//                    .padding(.top, -200)
+//                    .offset(x: -45)
+                //Book Title
+                RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.green.opacity(0.4)) // Adjust the color and opacity here
+                        .frame(width: 200, height: 50)
+                        
+                        .shadow(color: .black, radius: 10, x: 0, y: 0)
+                        .overlay(
+                            TextField("  Book Title",text:$title) // Display the title inside the rectangle
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .frame(width: 200)
+                                .shadow(color: .accentColor, radius: 10, x: 0, y: 0)
+                                .padding()
+                        )
                 
+                //Author
+                RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.green.opacity(0.4)) // Adjust the color and opacity here
+                        .frame(width: 200, height: 50)
+                        .shadow(color: .black, radius: 10, x: 0, y: 0)
+                        .overlay(
+                            TextField("  Author",text:$title) // Display the title inside the rectangle
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .frame(width: 200)
+                                .shadow(color: .accentColor, radius: 10, x: 0, y: 0)
+                                .padding()
+                        )
                 
-               
+                //Description
+                RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.green.opacity(0.4)) // Adjust the color and opacity here
+                        .frame(width: 200, height: 200)
+                        .shadow(color: .black, radius: 10, x: 0, y: 0)
+                        .overlay(
+                            
+                            
+                            TextEditor(text: $paragraphText) // Display the title inside the rectangle
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(.green)
+                                .frame(width: 200, height: 200)
+                                .onAppear{
+                                    UITextView.appearance().backgroundColor = .clear
+                                }
+                                .shadow(color: .accentColor, radius: 10, x: 0, y: 0)
+                                .padding()
+                        )
             }
+            .offset(x: -50, y: 25)
             
         }
         .ignoresSafeArea(.all)

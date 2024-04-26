c890f354b3f18e198897a5d6565b4a4be256aa5d
Melanie Nunez <Melaniejnunez@gmail.com>
2024-04-25 21:59:38 -0500

    BIOMIE-0101- Implemented all the images on the buttons

--- BiomeOfus/Views/CherryBlossomView.swift
+++ BiomeOfus/Views/CherryBlossomView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct CherryBlossomView: View {
+    @AppStorage("title") private var title: String = "BiomeOfUs" // Default title
+    @State private var showBiomeForm = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("cherry3")
+                .resizable()
+                .scaledToFill()
+                .modifier(MyCustomModifier())
+                .zIndex(0)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.green.opacity(0.4)) // Adjust the color and opacity here
+                        .frame(width: 200, height: 50)
+                        .offset(x: -150, y: -400)
+                        .shadow(color: .black, radius: 10, x: 0, y: 0)
+                        .overlay(
+                            TextField("BiomeOfUs",text:$title) // Display the title inside the rectangle
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .frame(width: 200)
+                                .offset(x: -150, y: -400)
+                                .shadow(color: .accentColor, radius: 10, x: 0, y: 0)
+                                .padding()
+                        )
+                        .offset(x:0, y:50)
+                
+                
+            }
+            .offset(x: 45, y: 125)
+        }
+        .ignoresSafeArea(.all)
+        
     }
 }
 
